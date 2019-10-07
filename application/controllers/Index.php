202b9462bd4ffe2111cabb5d424869cfdb78a0ba
belly <belly@mail.com>
2019-10-07 11:16:07 +0800

    demo_start

---
+++ application/controllers/Index.php
@@ -0,0 +1,8 @@
+<?php
+class IndexController extends Yaf_Controller_Abstract {
+   public function indexAction() {//默认Action
+	echo 111122222;exit;
+       # $this->getView()->assign("content", "Hello World");
+   }
+}
+?>
